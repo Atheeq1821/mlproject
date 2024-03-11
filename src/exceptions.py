3b609a5e4016d9cf1fcdf542b2f3f419193d54d4
Syed Atheequr Rahaman <syed21ad067@rmkcet.ac.in>
2024-03-11 22:39:18 +0530

    Data Ingestion Created

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,13 +1,21 @@
 import sys
-
+from src.logger import logging
 def handle_exception(error,error_details:sys):
     _,_,exc_tb=error_details.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_msg="Error Occured in the file name [{0}] in line numer[{1}] and the error message is [{2}]".format(file_name,exc_tb.tb_lineno,str(error))
     return error_msg
-class CustomeEsception(Exception):
+class CustomeException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=handle_exception(error_message,error_details=error_detail)
     def __str__(self) -> str:
         return self.error_message
+    
+
+'''if __name__=="__main__":
+    try:
+        a=2/0
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomeEsception(e,sys)'''
